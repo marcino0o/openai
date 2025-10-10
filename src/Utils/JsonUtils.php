80935e6fb12948cac97163b969c18366c774fe1f
Marcin Różagórski <marcinrozagorski@gmail.com>
2025-10-10 17:54:56 +0200

    fix: part 4 of code refactor after static code analize

--- src/Utils/JsonUtils.php
+++ src/Utils/JsonUtils.php
@@ -12,18 +12,18 @@ final readonly class JsonUtils
     /**
      * @throws InvalidArgumentException
      */
-    public static function decode(
+    public static function decode(// @phpstan-ignore missingType.iterableValue
         string $json,
         bool $assoc = true,
         int $depth = 512,
         int $options = JSON_THROW_ON_ERROR
     ): array {
         try {
-            $data = json_decode($json, $assoc, $depth, $options);
+            $data = json_decode($json, $assoc, $depth, $options); // @phpstan-ignore argument.type
         } catch (Throwable) {
             throw new InvalidArgumentException('Provided string must be a valid json');
         }
 
-        return $data;
+        return $data; // @phpstan-ignore return.type
     }
 }
